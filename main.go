9bc95dbce8744b49281d736bd8ce6aec9ff7efd3
Gregory Ganley <gcganley2854@gmail.com>
2019-07-26 19:57:44 -0400

    Remove redundent GET decl

--- main.go
+++ main.go
@@ -9,12 +9,11 @@ import (
 
 func main() {
 	r := chi.NewRouter()
-	r.Get("/", RootHandler)
+	r.Mount("/", http.FileServer(http.Dir("client/dist")))
 	r.Route("/api", func(r chi.Router) {
 		r.Post("/search", PostSearchHandler)
 	})
-	r.Mount("/", http.FileServer(http.Dir("client/dist")))
+
 	http.Handle("/", r)
-	// log.Fatal(http.ListenAndServe(":8080", r))
 	appengine.Main()
 }
